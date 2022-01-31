a9255d981fda512d79eda18129b7d1c9f07ecfea
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-01-31 16:49:55 -0200

    atualizando estilos

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,9 +5,9 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } width="200px" />
+      <div data-testid="planet-card" className="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
